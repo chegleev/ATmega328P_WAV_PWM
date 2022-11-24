193a4dbd02e11b9cef3f95e33b0b1cc60701caa2
chegleev <alekseypro@list.ru>
2022-11-24 18:56:51 +0300

    Update main.c

--- main.c
+++ main.c
@@ -33,39 +33,34 @@ ISR(TIMER1_COMPA_vect) {
 
 void startPlayback() {
 	DDRB |= 1 << 3;
-
-    ASSR &= ~(_BV(EXCLK) | _BV(AS2));
-
-    TCCR2A |= _BV(WGM21) | _BV(WGM20);
-    TCCR2B &= ~_BV(WGM22);
-
-    TCCR2A = (TCCR2A | _BV(COM2A1)) & ~_BV(COM2A0);
-    TCCR2A &= ~(_BV(COM2B1) | _BV(COM2B0));
-    TCCR2B = (TCCR2B & ~(_BV(CS12) | _BV(CS11))) | _BV(CS10);
-    OCR2A = pgm_read_byte(&sounddata_data[0]);
-
-    cli();
-
-    TCCR1B = (TCCR1B & ~_BV(WGM13)) | _BV(WGM12);
-    TCCR1A = TCCR1A & ~(_BV(WGM11) | _BV(WGM10));
-
-    TCCR1B = (TCCR1B & ~(_BV(CS12) | _BV(CS11))) | _BV(CS10);
-
-    OCR1A = F_CPU / SAMPLE_RATE;
-
-    TIMSK1 |= _BV(OCIE1A);
-
-    lastSample = pgm_read_byte(&sounddata_data[sounddata_length-1]);
-    sample = 0;
-    sei();
+	ASSR &= ~(_BV(EXCLK) | _BV(AS2));
+	
+	TCCR2A |= _BV(WGM21) | _BV(WGM20);
+	TCCR2B &= ~_BV(WGM22);
+	
+	TCCR2A = (TCCR2A | _BV(COM2A1)) & ~_BV(COM2A0);
+	TCCR2A &= ~(_BV(COM2B1) | _BV(COM2B0));
+	TCCR2B = (TCCR2B & ~(_BV(CS12) | _BV(CS11))) | _BV(CS10);
+	OCR2A = pgm_read_byte(&sounddata_data[0]);
+	
+	cli();
+	
+	TCCR1B = (TCCR1B & ~_BV(WGM13)) | _BV(WGM12);
+	TCCR1A = TCCR1A & ~(_BV(WGM11) | _BV(WGM10));
+	TCCR1B = (TCCR1B & ~(_BV(CS12) | _BV(CS11))) | _BV(CS10);
+	OCR1A = F_CPU / SAMPLE_RATE;
+	
+	TIMSK1 |= _BV(OCIE1A);
+	
+	lastSample = pgm_read_byte(&sounddata_data[sounddata_length-1]);
+	sei();
 }
 
 int main(void) {
-
-    startPlayback();
+	
+	startPlayback();
 
 	for(;;) {
-
 	}
 
 	return 0;
